691754fe0973172f528f37e9d003dfc432001a9a
Shane Patrick Roach <roach.patrick.shane@gmail.com>
2022-01-21 18:18:15 -0800

    added some lab 10

--- server.js
+++ server.js
@@ -1,25 +1,15 @@
 'use strict';
 
 
-//importing express - we need this because its an Express server
 const express = require('express');
 
-// how we use express as per the docs
 const app = express();
 
-// must import dotenv before using process.env.PORT
+const cors = require('cors');
+app.use(cors());
 require('dotenv').config();
-
-// use the port we want locally, and make this deployable
 const PORT = process.env.PORT || 3002;
 
-//must import cors to use it
-var cors = require('cors');
-// must USE cors to hit server from other apps
-app.use(cors());
-
-//import weather data from json
-//const weatherData = require('./data/weather.json');
 
 const getWeather = require('./modules/weather.js');
 const getMovies = require('./modules/movie.js');
@@ -27,7 +17,4 @@ const getMovies = require('./modules/movie.js');
 app.get('/weather', getWeather);
 app.get('/movies', getMovies);
 
-
-app.listen(PORT, () => console.log(`listening on port ${PORT}`));
-
-
+app.listen(PORT, () => console.log(`Server up on ${PORT}`));
